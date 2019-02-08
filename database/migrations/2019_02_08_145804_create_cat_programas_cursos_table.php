73f7e3aa50c365a3cfd09c861a9a5846abf629a2
Anderson Mendoza <amendozaaguiar@outlook.com>
2019-02-08 15:08:18 -0500

    Tabla de Plantillas y Cats con progrmas

---
+++ database/migrations/2019_02_08_145804_create_cat_programas_cursos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatProgramasCursosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cat_programas_cursos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('cat_id')
+                ->comment('Codigo del CAT');
+            $table->unsignedInteger('programa_id')
+                ->comment('Codigo del programa');
+                $table->unsignedInteger('curso_id')
+                ->comment('Codigo del curso');
+
+            $table->softDeletes(); 
+            $table->timestamps();
+
+            //Foraneas
+            $table->foreign('cat_id')->references('id')->on('cat');
+            $table->foreign('programa_id')->references('id')->on('programas');
+            $table->foreign('curso_id')->references('id')->on('cursos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cat_programas_cursos');
+    }
+}
